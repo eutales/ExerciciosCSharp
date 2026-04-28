d7706646ba1e20e15a11c5f3cae73902033e77ad
eutales <eu.tsiqueira@gmail.com>
2026-04-28 13:59:54 -0300

    Feito até o ex32

---
+++ Exercicio032/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Digite um número: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n <= 10)
+{
+    Console.WriteLine("F1");
+}
+
+if (n > 10)
+{
+    if (n <= 100)
+    {
+        Console.WriteLine("F2");
+    }
+}
+
+if (n > 100)
+{
+    Console.WriteLine("F3");
+}
+
+Console.WriteLine("Segundo teste");
+
+if (n <= 10)
+{
+    Console.WriteLine("F1");
+}
+else if (n <= 100)
+{
+    Console.WriteLine("F2");
+}
+else
+{
+    Console.WriteLine("F3");
+}
